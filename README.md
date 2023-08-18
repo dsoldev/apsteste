80e6048c61649714dc7c0b98ddc58694d847d12b
Diego <dpsoler09@gmail.com>
2023-08-18 12:08:25 -0300

    catchup

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # apsteste
+
+#alterado
--- test.py
+++ test.py
@@ -1,9 +1,23 @@
-import os
-import sys
+import openai
 
-# get env variables SECRET
-SECRET = os.environ.get('SECRET')
+def test_openai_authentication():
+    with open("openai_config.txt", "r") as f:
+        config = f.read().strip()
+    key = config.split('=')[1]
 
+    openai.api_key = key
 
-def test():
-    assert False, SECRET
+    # Make a simple completion request to test the authentication
+    response = openai.Completion.create(
+        engine="davinci",
+        prompt="Translate the following English text to French: 'Hello, how are you?'",
+        max_tokens=60
+    )
+
+    result = response.choices[0].text.strip()
+
+    # Save the result to feedback.txt
+    with open("feedback.txt", "w") as f:
+        f.write(result)
+
+    assert result == "Bonjour, comment ça va?", "OpenAI API call failed or returned unexpected result."
